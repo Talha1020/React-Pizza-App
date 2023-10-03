e0b5bf77d1a14c28477a907cf59435de751f280c
Talha Siddique <Talhasiddique375@gmail.com>
2023-10-03 23:33:00 +0200

    reworked with Tailwind CSS and added some buttons files

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
-import Order from "./order/Order";
-import Home from "./ui/Home";
-import CreateOrder from "./order/CreateOrder";
-import OrderItem from "./order/OrderItem";
-import Menu from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
-import AppLayout from "./AppLayout";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { loader as menuLoader } from "./features/menu/Menu";
-import Error from "./ui/Error";
-import { loadOrder as orderLoader } from "./order/Order";
-import { createNewOrder } from "./order/CreateOrder";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Home from './ui/Home';
+import Error from './ui/Error';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import AppLayout from './ui/AppLayout';
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     errorElement: <Error />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/order/:id", element: <Order />, loader: orderLoader },
 
-      { path: "/menu", element: <Menu />, loader: menuLoader },
-      { path: "/cart", element: <Cart /> },
+    children: [
       {
-        path: "/order/new",
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      { path: '/cart', element: <Cart /> },
+      {
+        path: '/order/new',
         element: <CreateOrder />,
-        action: createNewOrder,
+        action: createOrderAction,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
