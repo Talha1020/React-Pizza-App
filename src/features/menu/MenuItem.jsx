3c06a7a0e50a9fbfc51ca457f8f8f88b809b7d14
Talha Siddique <Talhasiddique375@gmail.com>
2023-10-06 23:51:05 +0200

    Fixed input item quantity, and synchronized addition and subtraction of quantities and price i the cart and cart overview bar

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,9 +1,56 @@
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import { addQuantity, addToCart } from '../cart/cartSlice';
+import { useEffect, useState } from 'react';
+
 /* eslint react/prop-types: 0 */
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const cart = useSelector((state) => state.cart.cart);
 
+  const dispatch = useDispatch();
+  const [ItemQuantity, setItemQuantity] = useState(0);
+
+  const newPizza = {
+    pizzaId: id,
+    name,
+    quantity: ItemQuantity,
+    unitPrice,
+    totalPrice: unitPrice * ItemQuantity,
+  };
+  function handlerOnClick() {
+    if (ItemQuantity === 0) return;
+    if (cart.length > 0) {
+      const containsItem = cart.some((item) => item.pizzaId === id);
+      containsItem
+        ? dispatch(addQuantity(newPizza))
+        : dispatch(addToCart(newPizza));
+    } else {
+      dispatch(addToCart(newPizza));
+    }
+
+    // ;
+  }
+
+  function quantityHandlerAdd() {
+    setItemQuantity((ItemQuantity) => ItemQuantity + 1);
+  }
+  // useEffect(
+  //   function () {
+  //     dispatch(addToCart(newPizza));
+  //   },
+  //   [ItemQuantity, dispatch],
+  // );
+
+  function quantityHandlerSub() {
+    if (ItemQuantity > 0) setItemQuantity((ItemQuantity) => ItemQuantity - 1);
+  }
+  function quantityHandler(e) {
+    const inputValue = Number(e.target.value);
+    if (isNaN(inputValue)) return;
+    setItemQuantity(inputValue);
+  }
   return (
     <li className="flex gap-4 py-2">
       <img
@@ -25,7 +72,34 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <>
+              <div className="ml-[22rem] flex items-center">
+                <button
+                  onClick={quantityHandlerAdd}
+                  className="relative flex h-6 w-6 items-center justify-center rounded-full bg-gray-300 text-sm font-bold text-gray-800 transition duration-300 ease-in-out hover:bg-gray-400  focus:outline-none"
+                >
+                  <span className="absolute inset-0 ">+</span>
+                </button>
+                <input
+                  type="text"
+                  className="mx-2 h-6 w-10 rounded-md border border-gray-300 text-center text-gray-700 focus:outline-none"
+                  value={ItemQuantity}
+                  inputMode="numeric"
+                  onChange={quantityHandler}
+                />
+                <button
+                  onClick={quantityHandlerSub}
+                  className="relative flex h-6 w-6 items-center justify-center rounded-full bg-gray-300 text-sm font-bold text-gray-800 transition duration-300 ease-in-out hover:bg-gray-400  focus:outline-none"
+                >
+                  <span className="absolute inset-0 ">-</span>
+                </button>
+              </div>
+              <Button onClick={handlerOnClick} type="small">
+                Add to cart
+              </Button>
+            </>
+          )}
         </div>
       </div>
     </li>
