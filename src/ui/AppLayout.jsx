cb5b28d5621f138822381ee8bf8f5ec4faecfac3
Talha Siddique <Talhasiddique375@gmail.com>
2023-10-11 01:51:15 +0200

    updated

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -16,7 +16,7 @@ function AppLayout() {
       <Header />
 
       <div className="overflow-auto">
-        <main className="mx-auto max-w-3xl">
+        <main className="relative mx-auto max-w-3xl">
           <Outlet />
         </main>
       </div>
