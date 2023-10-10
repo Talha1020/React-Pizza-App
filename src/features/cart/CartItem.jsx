0b2475a0fac128de65da661e2cde5d49e073aaa9
Talha Siddique <Talhasiddique375@gmail.com>
2023-10-10 20:36:30 +0200

    dispatch function bug resolved

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,70 +1,38 @@
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
 /* eslint react/prop-types: 0 */
-import { useDispatch, useSelector } from 'react-redux';
-import { addQuantity, addToCart, delQuantity } from './cartSlice';
-function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
-  // const [ItemQuantity, setItemQuantity] = useState(0);
+import { useDispatch } from 'react-redux';
+import { addQuantity, delFromCart, delQuantity } from './cartSlice';
 
-  // const newPizza = {
-  //   pizzaId: id,
-  //   name,
-  //   quantity: ItemQuantity,
-  //   unitPrice,
-  //   totalPrice: unitPrice * ItemQuantity,
-  // };
-  // function handlerOnClick() {
-  //   if (ItemQuantity === 0) return;
-  //   if (cart.length > 0) {
-  //     const containsItem = cart.some((item) => item.pizzaId === id);
-  //     containsItem
-  //       ? dispatch(addQuantity(newPizza))
-  //       : dispatch(addToCart(newPizza));
-  //   } else {
-  //     dispatch(addToCart(newPizza));
-  //   }
-  // function quantityHandlerAdd() {
-  //   setItemQuantity((ItemQuantity) => ItemQuantity + 1);
-  // }
-  // function quantityHandlerSub() {
-  //   if (ItemQuantity > 0) setItemQuantity((ItemQuantity) => ItemQuantity - 1);
-  // }
+function CartItem({ item }) {
+  const { name, quantity, totalPrice } = item;
+
   const dispatch = useDispatch();
-  const quantityId = useSelector((state) => state.cart.cart.pizzaId);
-  console.log(quantityId);
-  function deleteHandler() {
-    dispatch(delQuantity(quantityId));
-  }
+
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
-      <p className="mb-1 sm:mb-0">
-        {quantity}&times; {name}
+      <p className="mb-1  sm:mb-0">
+        <span className="font-semibold">{quantity}</span>
+        &times; {name}
       </p>
-      <div className="flex items-center justify-between sm:gap-6">
-        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <div className="ml-[22rem] flex items-center">
+      <div className="flex  items-center justify-between  sm:gap-2">
+        <p className=" mr-5 text-sm font-bold ">{formatCurrency(totalPrice)}</p>
+        <div className="flex items-center space-x-1">
           <button
-            // onClick={quantityHandlerAdd}
+            onClick={() => dispatch(addQuantity(item))}
             className="relative flex h-6 w-6 items-center justify-center rounded-full bg-gray-300 text-sm font-bold text-gray-800 transition duration-300 ease-in-out hover:bg-gray-400  focus:outline-none"
           >
             <span className="absolute inset-0 ">+</span>
           </button>
-          <input
-            type="text"
-            className="mx-2 h-6 w-10 rounded-md border border-gray-300 text-center text-gray-700 focus:outline-none"
-            // value={ItemQuantity}
-            inputMode="numeric"
-            // onChange={quantityHandler}
-          />
+
           <button
-            // onClick={quantityHandlerSub}
+            onClick={() => dispatch(delFromCart(item))}
             className="relative flex h-6 w-6 items-center justify-center rounded-full bg-gray-300 text-sm font-bold text-gray-800 transition duration-300 ease-in-out hover:bg-gray-400  focus:outline-none"
           >
             <span className="absolute inset-0 ">-</span>
           </button>
         </div>
-        <Button onClick={deleteHandler} type="small">
+        <Button onClick={() => dispatch(delQuantity(item))} type="small">
           Delete
         </Button>
       </div>
